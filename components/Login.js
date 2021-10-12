9d93f6be711520c3e3e0f5488db137f0ce1de65a
coscoaj1 <coscoanthony1@gmail.com>
2021-10-12 18:58:34 -0400

    auth

--- components/Login.js
+++ components/Login.js
@@ -1,10 +1,20 @@
-import React, { useState } from 'react';
-import firebaseClient from '../firebase/firebaseClient';
-import firebase from 'firebase/app';
-import 'firebase/auth';
+import React, { useState } from "react";
+import firebaseClient from "../firebase/firebaseClient";
+import firebase from "firebase/app";
+import "firebase/auth";
+import cancelX from "../public/images/x-svgrepo-com (1).svg";
 
 function Login() {
-	return <div className="flex flex-col h-auto w-96"></div>;
+  return (
+    <div className="flex flex-col h-auto w-96">
+      <header className="flex flex-0 items-center">
+        <button className="bg-gray-100 rounded-full">
+          <cancelX width={18} className="px-1 py-1"></cancelX>
+        </button>
+        <div>Log in or sign up</div>
+      </header>
+    </div>
+  );
 }
 
 export default Login;
