56c0bb5e078e28f346146c637d3b9440a38e8682
Anthony Cosco <coscoanthony1@gmail.com>
2021-10-14 06:00:19 -0400

    login modal opening/closing properly

--- components/LoginModal.js
+++ components/LoginModal.js
@@ -1,57 +1,52 @@
-import React, { useState } from "react";
-import { Dialog } from "@headlessui/react";
-import CancelIcon from "../public/images/x-svgrepo-com (1).svg";
+import React, { useState } from 'react';
+import { Dialog } from '@headlessui/react';
+import CancelIcon from '../public/images/x-svgrepo-com (1).svg';
 
-function LoginModal() {
-  let [isOpen, setIsOpen] = useState(true);
-
-  const handleClose = () => {
-    setIsOpen(false);
-  };
-  return (
-    <div>
-      <Dialog
-        className="bg-white mx-auto my-auto text-black z-30 fixed inset-0 max-w-lg h-lg rounded-lg"
-        open={isOpen}
-        onClose={() => setIsOpen(false)}
-      >
-        <Dialog.Overlay />
-        <Dialog.Title className="flex w-full items-center justify-between ">
-          <button
-            onClick={handleClose}
-            className="text-left ml-4 rounded-full hover:bg-gray-200 w-6 h-6 border-none"
-          >
-            <CancelIcon className="pl-1" width="20" height="20" />
-          </button>
-          <div className="font-bold mr-48 py-4">Log in or sign up</div>
-        </Dialog.Title>
-        <div className="border-b border-gray-200"></div>
-        <Dialog.Description className="text-3xl font-medium py-4 ml-4">
-          Welcome to Airbnb
-        </Dialog.Description>
-        <div className="flex flex-col">
-          <label for="email" className="pl-4">
-            Email:
-          </label>
-          <input
-            id="email"
-            type="email"
-            className="w-lg mx-4 py-1 pl-1 border border-black rounded-md"
-          />
-          <label for="password" className="pl-4">
-            Password:
-          </label>
-          <input
-            id="password"
-            type="password"
-            className="w-lg ml-4 mr-4 py-1 pl-1 border border-black rounded-md"
-          />
-          <button onClick={() => setIsOpen(false)}>Deactivate</button>
-          <button onClick={() => setIsOpen(false)}>Cancel</button>
-        </div>
-      </Dialog>
-    </div>
-  );
+function LoginModal({ open, onClose }) {
+	return (
+		<div>
+			<Dialog
+				className="bg-white mx-auto my-auto text-black z-30 fixed inset-0 max-w-lg h-lg rounded-lg"
+				open={open}
+				onClose={onClose}
+			>
+				<Dialog.Overlay />
+				<Dialog.Title className="flex w-full items-center justify-between ">
+					<button
+						onClick={onClose}
+						className="text-left ml-4 rounded-full hover:bg-gray-200 w-6 h-6 border-none"
+					>
+						<CancelIcon className="pl-1" width="20" height="20" />
+					</button>
+					<div className="font-bold mr-48 py-4">Log in or sign up</div>
+				</Dialog.Title>
+				<div className="border-b border-gray-200"></div>
+				<Dialog.Description className="text-3xl font-medium py-4 ml-4">
+					Welcome to Airbnb
+				</Dialog.Description>
+				<div className="flex flex-col">
+					<label for="email" className="pl-4">
+						Email:
+					</label>
+					<input
+						id="email"
+						type="email"
+						className="w-lg mx-4 py-1 pl-1 border border-black rounded-md"
+					/>
+					<label for="password" className="pl-4">
+						Password:
+					</label>
+					<input
+						id="password"
+						type="password"
+						className="w-lg ml-4 mr-4 py-1 pl-1 border border-black rounded-md"
+					/>
+					<button onClick={() => setIsOpen(false)}>Deactivate</button>
+					<button onClick={() => setIsOpen(false)}>Cancel</button>
+				</div>
+			</Dialog>
+		</div>
+	);
 }
 
 export default LoginModal;
--- components/UserDropDown.js
+++ components/UserDropDown.js
@@ -1,110 +1,116 @@
-import React, { useState } from "react";
-import { Menu } from "@headlessui/react";
-import Image from "next/image";
-import userIcon from "../public/images/profile-user.png";
-import menuIcon from "../public/images/icons8-menu-24.png";
-import LoginModal from "./LoginModal";
+import React, { useState } from 'react';
+import { Menu } from '@headlessui/react';
+import Image from 'next/image';
+import userIcon from '../public/images/profile-user.png';
+import menuIcon from '../public/images/icons8-menu-24.png';
+import LoginModal from './LoginModal';
 
 export default function UserDropDown({ navbar }) {
-  const [showModal, setShowModal] = useState(false);
+	const [showModal, setShowModal] = useState(false);
+	const [isOpen, setIsOpen] = useState(true);
 
-  const onClick = () => {
-    setShowModal(true);
-  };
+	const onClick = () => {
+		setShowModal(true);
+		setIsOpen(true);
+	};
 
-  return (
-    <>
-      <div className="-mr-6">
-        <Menu>
-          <Menu.Button>
-            <div
-              className={
-                navbar
-                  ? "flex flex-row items-center mr-6 bg-white rounded-full border hover:shadow-md"
-                  : "flex flex-row items-center mr-6 bg-white rounded-full border"
-              }
-            >
-              <div className="w-5 h-auto mt-2 mb-1 ml-2 z-10">
-                <Image src={menuIcon} width={16} height={18} alt="" />
-              </div>
-              <div className="w-7 mx-2 mt-1.5">
-                <Image src={userIcon} alt="" />
-              </div>
-            </div>
-          </Menu.Button>
-          <Menu.Items className="z-10 absolute right-0 w-64 h-auto mt-2 origin-top-right bg-white text-black divide-y divide-gray-100 rounded-xl shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
-            <div className="flex flex-col px-1 py-1 border-b border-gray-200 text-sm">
-              <Menu.Item>
-                {({ active }) => (
-                  <li
-                    as=""
-                    className={`${
-                      active ? " bg-gray-100" : "bg-white"
-                    } w-full font-medium px-3 py-2.5 mx-0 rounded-lg list-none`}
-                  >
-                    <button onClick={onClick}>Sign up</button>
-                  </li>
-                )}
-              </Menu.Item>
-              <Menu.Item>
-                {({ active }) => (
-                  <a
-                    as=""
-                    className={`${
-                      active ? "bg-gray-100" : "bg-white"
-                    } w-full px-3 py-2.5 mx-0 rounded-lg`}
-                    href="#"
-                  >
-                    Log in
-                  </a>
-                )}
-              </Menu.Item>
-            </div>
-            <div className="flex flex-col px-1 py-1 text-sm">
-              <Menu.Item>
-                {({ active }) => (
-                  <a
-                    as=""
-                    className={`${
-                      active ? "bg-gray-100" : "bg-white"
-                    } w-full px-3 py-2.5 mx-0 rounded-lg`}
-                    href="#"
-                  >
-                    Host your home
-                  </a>
-                )}
-              </Menu.Item>
-              <Menu.Item>
-                {({ active }) => (
-                  <a
-                    as=""
-                    className={`${
-                      active ? "bg-gray-100" : "bg-white"
-                    } w-full px-3 py-2.5 mx-0 rounded-lg`}
-                    href="#"
-                  >
-                    Host an experience
-                  </a>
-                )}
-              </Menu.Item>
-              <Menu.Item>
-                {({ active }) => (
-                  <a
-                    as=""
-                    className={`${
-                      active ? "bg-gray-100" : "bg-white"
-                    } w-full px-3 py-2.5 mx-0 rounded-lg`}
-                    href="#"
-                  >
-                    Help
-                  </a>
-                )}
-              </Menu.Item>
-            </div>
-          </Menu.Items>
-        </Menu>
-      </div>
-      {showModal && <LoginModal />}
-    </>
-  );
+	const handleClose = () => {
+		setIsOpen(false);
+	};
+
+	return (
+		<>
+			<div className="-mr-6">
+				<Menu>
+					<Menu.Button>
+						<div
+							className={
+								navbar
+									? 'flex flex-row items-center mr-6 bg-white rounded-full border hover:shadow-md'
+									: 'flex flex-row items-center mr-6 bg-white rounded-full border'
+							}
+						>
+							<div className="w-5 h-auto mt-2 mb-1 ml-2 z-10">
+								<Image src={menuIcon} width={16} height={18} alt="" />
+							</div>
+							<div className="w-7 mx-2 mt-1.5">
+								<Image src={userIcon} alt="" />
+							</div>
+						</div>
+					</Menu.Button>
+					<Menu.Items className="z-10 absolute right-0 w-64 h-auto mt-2 origin-top-right bg-white text-black divide-y divide-gray-100 rounded-xl shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+						<div className="flex flex-col px-1 py-1 border-b border-gray-200 text-sm">
+							<Menu.Item>
+								{({ active }) => (
+									<li
+										as=""
+										className={`${
+											active ? ' bg-gray-100' : 'bg-white'
+										} w-full font-medium px-3 py-2.5 mx-0 rounded-lg list-none`}
+									>
+										<button onClick={onClick}>Sign up</button>
+									</li>
+								)}
+							</Menu.Item>
+							<Menu.Item>
+								{({ active }) => (
+									<a
+										as=""
+										className={`${
+											active ? 'bg-gray-100' : 'bg-white'
+										} w-full px-3 py-2.5 mx-0 rounded-lg`}
+										href="#"
+									>
+										Log in
+									</a>
+								)}
+							</Menu.Item>
+						</div>
+						<div className="flex flex-col px-1 py-1 text-sm">
+							<Menu.Item>
+								{({ active }) => (
+									<a
+										as=""
+										className={`${
+											active ? 'bg-gray-100' : 'bg-white'
+										} w-full px-3 py-2.5 mx-0 rounded-lg`}
+										href="#"
+									>
+										Host your home
+									</a>
+								)}
+							</Menu.Item>
+							<Menu.Item>
+								{({ active }) => (
+									<a
+										as=""
+										className={`${
+											active ? 'bg-gray-100' : 'bg-white'
+										} w-full px-3 py-2.5 mx-0 rounded-lg`}
+										href="#"
+									>
+										Host an experience
+									</a>
+								)}
+							</Menu.Item>
+							<Menu.Item>
+								{({ active }) => (
+									<a
+										as=""
+										className={`${
+											active ? 'bg-gray-100' : 'bg-white'
+										} w-full px-3 py-2.5 mx-0 rounded-lg`}
+										href="#"
+									>
+										Help
+									</a>
+								)}
+							</Menu.Item>
+						</div>
+					</Menu.Items>
+				</Menu>
+			</div>
+			{showModal && <LoginModal open={isOpen} onClose={handleClose} />}
+		</>
+	);
 }
