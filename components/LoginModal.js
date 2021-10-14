00d50669411ee5a6c602ad71e08cd7740aecfa45
coscoaj1 <coscoanthony1@gmail.com>
2021-10-14 05:28:39 -0400

    login form

--- components/LoginModal.js
+++ components/LoginModal.js
@@ -1,30 +1,57 @@
-import React, { useState } from 'react';
-import { Dialog } from '@headlessui/react';
+import React, { useState } from "react";
+import { Dialog } from "@headlessui/react";
+import CancelIcon from "../public/images/x-svgrepo-com (1).svg";
 
 function LoginModal() {
-	let [isOpen, setIsOpen] = useState(true);
+  let [isOpen, setIsOpen] = useState(true);
 
-	return (
-		<div>
-			<Dialog
-				className="bg-white mx-auto my-auto text-black z-30 fixed inset-0 w-96 h-96"
-				open={isOpen}
-				onClose={() => setIsOpen(false)}
-			>
-				<Dialog.Overlay />
-				<Dialog.Title>Deactivate account</Dialog.Title>
-				<Dialog.Description>
-					This will permanently deactivate your account
-				</Dialog.Description>
-				<p>
-					Are you sure you want to deactivate your account? All of your data
-					will be permanently removed. This action cannot be undone.
-				</p>
-				<button onClick={() => setIsOpen(false)}>Deactivate</button>
-				<button onClick={() => setIsOpen(false)}>Cancel</button>
-			</Dialog>
-		</div>
-	);
+  const handleClose = () => {
+    setIsOpen(false);
+  };
+  return (
+    <div>
+      <Dialog
+        className="bg-white mx-auto my-auto text-black z-30 fixed inset-0 max-w-lg h-lg rounded-lg"
+        open={isOpen}
+        onClose={() => setIsOpen(false)}
+      >
+        <Dialog.Overlay />
+        <Dialog.Title className="flex w-full items-center justify-between ">
+          <button
+            onClick={handleClose}
+            className="text-left ml-4 rounded-full hover:bg-gray-200 w-6 h-6 border-none"
+          >
+            <CancelIcon className="pl-1" width="20" height="20" />
+          </button>
+          <div className="font-bold mr-48 py-4">Log in or sign up</div>
+        </Dialog.Title>
+        <div className="border-b border-gray-200"></div>
+        <Dialog.Description className="text-3xl font-medium py-4 ml-4">
+          Welcome to Airbnb
+        </Dialog.Description>
+        <div className="flex flex-col">
+          <label for="email" className="pl-4">
+            Email:
+          </label>
+          <input
+            id="email"
+            type="email"
+            className="w-lg mx-4 py-1 pl-1 border border-black rounded-md"
+          />
+          <label for="password" className="pl-4">
+            Password:
+          </label>
+          <input
+            id="password"
+            type="password"
+            className="w-lg ml-4 mr-4 py-1 pl-1 border border-black rounded-md"
+          />
+          <button onClick={() => setIsOpen(false)}>Deactivate</button>
+          <button onClick={() => setIsOpen(false)}>Cancel</button>
+        </div>
+      </Dialog>
+    </div>
+  );
 }
 
 export default LoginModal;
--- pages/index.js
+++ pages/index.js
@@ -1,36 +1,36 @@
-import React, { useState, useEffect } from 'react';
-import homeService from '../services/homes';
-import Discover from '../components/Discover';
-import Bottombar from '../components/Bottombar';
-import LiveAnywhere from '../components/LiveAnywhere';
-import NearbyCardList from '../components/NearbyCardList';
-import Hero from '../components/Hero';
-import Header from '../components/Header';
-import TryHosting from '../components/TryHosting';
-import TabsList from '../components/TabsList';
-import Footer from '../components/Footer';
-import LoginModal from '../components/LoginModal';
-import { useAuth } from '../firebase/auth';
+import React, { useState, useEffect } from "react";
+import homeService from "../services/homes";
+import Discover from "../components/Discover";
+import Bottombar from "../components/Bottombar";
+import LiveAnywhere from "../components/LiveAnywhere";
+import NearbyCardList from "../components/NearbyCardList";
+import Hero from "../components/Hero";
+import Header from "../components/Header";
+import TryHosting from "../components/TryHosting";
+import TabsList from "../components/TabsList";
+import Footer from "../components/Footer";
+import LoginModal from "../components/LoginModal";
+import { useAuth } from "../firebase/auth";
 
 export default function Home() {
-	const { user } = useAuth();
+  const { user } = useAuth();
 
-	return (
-		<div className="font-circular box-border">
-			<LoginModal />
-			<Header />
-			<Hero />
-			<Bottombar />
-			<div className="px-14">
-				<NearbyCardList />
-				<LiveAnywhere />
-				<TryHosting />
-				<Discover />
-				<TabsList />
-			</div>
-			<Footer />
-		</div>
-	);
+  return (
+    <div className="font-circular box-border">
+      {/* <LoginModal /> */}
+      <Header />
+      <Hero />
+      <Bottombar />
+      <div className="px-14">
+        <NearbyCardList />
+        <LiveAnywhere />
+        <TryHosting />
+        <Discover />
+        <TabsList />
+      </div>
+      <Footer />
+    </div>
+  );
 }
 // useEffect(() => {
 // 	homeService.getAll().then((homes) => setHomes(homes));
