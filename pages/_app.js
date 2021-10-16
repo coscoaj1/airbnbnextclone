0d486378ff7041edeedefc6a26567070fb5e8b56
coscoaj1 <coscoanthony1@gmail.com>
2021-10-16 14:17:44 -0400

    localStorage

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,12 @@
-import { Provider } from 'next-auth/client';
-import '../styles/globals.css';
+import { Provider } from "next-auth/client";
+import "../styles/globals.css";
 
 function MyApp({ Component, pageProps: { session, ...pageProps } }) {
-	return (
-		<Provider session={session}>
-			<Component {...pageProps} />
-		</Provider>
-	);
+  return (
+    <Provider session={session}>
+      <Component {...pageProps} />
+    </Provider>
+  );
 }
 
 export default MyApp;
