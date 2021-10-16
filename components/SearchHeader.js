5fdcfd2e897cb17fef0551f95eb594b0d3896c24
Anthony Cosco <coscoanthony1@gmail.com>
2021-10-16 19:48:20 -0400

    searchHeader

--- components/SearchHeader.js
+++ components/SearchHeader.js
@@ -1,23 +1,25 @@
-import React, { useState } from "react";
-import Image from "next/image";
-import logo from "../public/images/airbnb-48.ico";
-import redLogo from "../public/images/airbnb-red-icon.png";
-import searchIcon from "../public/images/search-12-32.png";
-import globe from "../public/images/icons8-globe-50.png";
-import blackGlobe from "../public/images/icons8-globe-24.png";
-import UserDropDown from "./UserDropDown";
+import React, { useState } from 'react';
+import Image from 'next/image';
+import logo from '../public/images/airbnb-48.ico';
+import redLogo from '../public/images/airbnb-red-icon.png';
+import searchIcon from '../public/images/search-12-32.png';
+import globe from '../public/images/icons8-globe-50.png';
+import blackGlobe from '../public/images/icons8-globe-24.png';
+import UserDropDown from './UserDropDown';
 
 function SearchHeader() {
-  return (
-    <header className="ml-14 flex w-full h-30">
-      <button className="flex-5 flex flex-row items-center">
-        <div className="flex ">
-          <Image src={redLogo} width={32} height={32} alt="logo" />
-        </div>
-        <div className="hidden md:block font-medium text-2xl pl-1">airbnb</div>
-      </button>
-    </header>
-  );
+	return (
+		<header className="ml-14 flex w-full items-center">
+			<button className="flex flex-row items-center my-4">
+				<div className="flex ">
+					<Image src={redLogo} width={32} height={32} alt="logo" />
+				</div>
+			</button>
+			<div className="hidden md:block font-medium text-2xl ml-4  pl-4">
+				airbnb
+			</div>
+		</header>
+	);
 }
 
 export default SearchHeader;
--- pages/homes/index.js
+++ pages/homes/index.js
@@ -1,31 +1,32 @@
-import React, { useState, useEffect } from "react";
-import { useRouter } from "next/router";
-import Footer from "../../components/Footer";
-import SearchHeader from "../../components/searchHeader";
+import React, { useState, useEffect } from 'react';
+import { useRouter } from 'next/router';
+import Footer from '../../components/Footer';
+import SearchHeader from '../../components/SearchHeader';
 
 function HomeListings() {
-  const router = useRouter();
-  const { startDate, endDate, location, totalGuests } = router.query;
+	const router = useRouter();
+	const { startDate, endDate, location, totalGuests } = router.query;
 
-  const formattedStartDate = startDate.split(",")[0];
-  const formattedEndDate = endDate.split(",")[0];
+	const formattedStartDate = startDate.split(',')[0];
+	const formattedEndDate = endDate.split(',')[0];
+	const formattedLocation = location.split(',')[0];
 
-  return (
-    <>
-      <SearchHeader />
-      <div className="ml-14">
-        <section>
-          <div className="text-sm text-gray-900">
-            18 stays · {formattedStartDate} - {formattedEndDate} · {totalGuests}{" "}
-            guests
-          </div>
-          <div className="text-3xl font-bold">Stays in {location}</div>
-          <div>home3</div>
-        </section>
-        <Footer />
-      </div>
-    </>
-  );
+	return (
+		<>
+			<SearchHeader />
+			<div className="ml-14">
+				<section>
+					<div className="text-sm text-gray-900">
+						18 stays · {formattedStartDate} - {formattedEndDate} · {totalGuests}{' '}
+						guests
+					</div>
+					<div className="text-3xl font-bold">Stays in {formattedLocation}</div>
+					<div>home3</div>
+				</section>
+				<Footer />
+			</div>
+		</>
+	);
 }
 
 export default HomeListings;
