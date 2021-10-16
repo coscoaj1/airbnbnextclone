586f6c27465603539f53ee9ed8cbc8c7211c2d80
coscoaj1 <coscoanthony1@gmail.com>
2021-10-16 16:55:17 -0400

    search page

--- pages/homes/index.js
+++ pages/homes/index.js
@@ -1,28 +1,27 @@
-import React, { useState, useEffect } from 'react';
-import { useRouter } from 'next/router';
-import Header from '../../components/Header';
-import Footer from '../../components/Footer';
+import React, { useState, useEffect } from "react";
+import { useRouter } from "next/router";
+import Header from "../../components/Header";
+import Footer from "../../components/Footer";
 
 function HomeListings() {
-	const router = useRouter();
-	const { startDate, endDate, location, totalGuests } = router.query;
+  const router = useRouter();
+  const { startDate, endDate, location, totalGuests } = router.query;
 
-	const formattedStartDate = startDate.split(',')[0];
-	const formattedEndDate = endDate.split(',')[0];
+  const formattedStartDate = startDate.split(",")[0];
+  const formattedEndDate = endDate.split(",")[0];
 
-	return (
-		<div>
-			<Header />
-			<div className="text-lg">
-				Homes available in {location} for {totalGuests} guests between{' '}
-				{formattedStartDate} and {formattedEndDate}
-			</div>
+  return (
+    <div className="ml-14">
+      <div className="text-sm text-gray-900">
+        18 stays · {formattedStartDate} - {formattedEndDate} · {totalGuests}{" "}
+        guests
+      </div>
 
-			<div>home2</div>
-			<div>home3</div>
-			<Footer />
-		</div>
-	);
+      <div className="text-3xl font-bold">Stays in {location}</div>
+      <div>home3</div>
+      <Footer />
+    </div>
+  );
 }
 
 export default HomeListings;
