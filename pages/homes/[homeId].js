17c8b503d7b1bd957c4468a79018808021e3f92c
coscoaj1 <coscoanthony1@gmail.com>
2021-10-16 10:31:25 -0400

    routes

--- pages/homes/[homeId].js
+++ pages/homes/[homeId].js
@@ -1,10 +1,16 @@
-import React from 'react';
-import { useRouter } from 'next/router';
+import React from "react";
+import { useRouter } from "next/router";
 
 function homeDetail() {
-	const router = useRouter();
-	const homeId = router.query.homeId;
-	return <h1 className="text-4xl font-medium">Details about home {homeId}</h1>;
+  const router = useRouter();
+  const homeId = router.query.homeId;
+  return (
+    <div className="w-screen h-screen bg-black">
+      <h1 className="text-4xl text-purple-700 font-medium">
+        Details about home {homeId}
+      </h1>
+    </div>
+  );
 }
 
 export default homeDetail;
