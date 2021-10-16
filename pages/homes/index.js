0a98b28c6722c3f6b875a0e646b9d637003f9278
coscoaj1 <coscoanthony1@gmail.com>
2021-10-16 14:17:52 -0400

    local

--- pages/homes/index.js
+++ pages/homes/index.js
@@ -1,13 +1,26 @@
-import React from 'react';
+import React, { useState, useEffect } from "react";
 
+function getStoredData() {
+  const homesList = localStorage.getItem("searchParams");
+  return JSON.parse(homesList);
+}
 function HomeListings() {
-	return (
-		<div>
-			<div>home1</div>
-			<div>home2</div>
-			<div>home3</div>
-		</div>
-	);
+  const [homes, setHomes] = useState(localStorage.getItem("searchParams"));
+  console.log(homes);
+  useEffect(() => {
+    localStorage.setItem("searchParams", JSON.stringify(homes));
+  }, [homes]);
+  return (
+    <div>
+      <div>
+        Homes available in {homes.location} for {homes.guestCount} guest between{" "}
+        {homes.selectedDates[0].toLocaleString().split("T")[0]} and{" "}
+        {homes.selectedDates[1].toLocaleString().split("T")[0]}
+      </div>
+      <div>home2</div>
+      <div>home3</div>
+    </div>
+  );
 }
 
 export default HomeListings;
--- pages/index.js
+++ pages/index.js
@@ -1,34 +1,40 @@
-import React, { useState, useEffect } from 'react';
-import homeService from '../services/homes';
-import Discover from '../components/Discover';
-import Bottombar from '../components/Bottombar';
-import LiveAnywhere from '../components/LiveAnywhere';
-import NearbyCardList from '../components/NearbyCardList';
-import Hero from '../components/Hero';
-import Header from '../components/Header';
-import TryHosting from '../components/TryHosting';
-import TabsList from '../components/TabsList';
-import Footer from '../components/Footer';
-import { useSession } from 'next-auth/client';
+import React, { useState, useEffect, createContext, useContext } from "react";
+import homeService from "../services/homes";
+import Discover from "../components/Discover";
+import Bottombar from "../components/Bottombar";
+import LiveAnywhere from "../components/LiveAnywhere";
+import NearbyCardList from "../components/NearbyCardList";
+import Hero from "../components/Hero";
+import Header from "../components/Header";
+import TryHosting from "../components/TryHosting";
+import TabsList from "../components/TabsList";
+import Footer from "../components/Footer";
+import { useSession } from "next-auth/client";
 
 export default function Home() {
-	const [session, loading] = useSession();
-	console.log({ session, loading });
-	return (
-		<div className="font-circular box-border">
-			<Header />
-			<Hero />
-			<Bottombar />
-			<div className="px-14">
-				<NearbyCardList />
-				<LiveAnywhere />
-				<TryHosting />
-				<Discover />
-				<TabsList />
-			</div>
-			<Footer />
-		</div>
-	);
+  const [session, loading] = useSession();
+  const [searchData, setSearchData] = useState();
+  const sendSearchParams = (searchParams) => {
+    setSearchData(searchParams);
+  };
+  console.log(searchData);
+
+  console.log({ session, loading });
+  return (
+    <div className="font-circular box-border">
+      <Header sendSearchParams={sendSearchParams} />
+      <Hero />
+      <Bottombar />
+      <div className="px-14">
+        <NearbyCardList />
+        <LiveAnywhere />
+        <TryHosting />
+        <Discover />
+        <TabsList />
+      </div>
+      <Footer />
+    </div>
+  );
 }
 // useEffect(() => {
 // 	homeService.getAll().then((homes) => setHomes(homes));
