5db2c51b9ee0a4743e9574c41694e2c901825ffa
Anthony Cosco <coscoanthony1@gmail.com>
2021-10-17 20:11:36 -0400

    list detail page updates

--- components/HomeDetail.js
+++ components/HomeDetail.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import Image from 'next/image';
+import star from '../public/images/icons8-star-50.png';
 
-function HomeDetail({ item }) {
+function HomeDetail({ item, dayCount }) {
 	return (
 		<div>
 			<div className="border-t border-gray-300 my-5"></div>
@@ -14,9 +15,44 @@ function HomeDetail({ item }) {
 				/>
 				<div className="flex flex-col ml-4">
 					<div className="flex flex-col">
-						<div className="text-gray-600 text-sm">{item.description}</div>
+						<div className="text-gray-500 text-smbase">{item.description}</div>
 						<div className=" text-lg">{item.title}</div>
 						<div className="border-b border-gray-300 w-10 py-1"></div>
+					</div>
+
+					<div className="flex flex-col my-1 text-smbase ">
+						<div className="text-gray-500">
+							{item.guests} guests · {item.bedrooms} bedrooms · {item.beds} beds
+							· {item.baths} baths
+						</div>
+						<div className="text-gray-500">
+							{item.parking && <span>Free parking </span>}
+							{item.wifi && <span>· Wifi </span>}
+							{item.air_conditioning && <span>· Air Conditioning</span>}
+						</div>
+						<div className="mt-16 flex justify-between w-82">
+							<div className="flex flex-row items-end">
+								<div>
+									<Image className="" src={star} width={14} height={14} />
+								</div>
+								<div>
+									<span className="pl-1 font-medium">{item.rating}</span>
+									{item.reviews > 0 && (
+										<span className="text-smbase text-gray-500">
+											({item.reviews} reviews)
+										</span>
+									)}
+								</div>
+							</div>
+							<div className="flex flex-col items-end">
+								<div className="text-lg">
+									<span className="font-bold">${item.price}</span> / night
+								</div>
+								<div className="underline text-smbase text-gray-500">
+									${dayCount * item.price} total
+								</div>
+							</div>
+						</div>
 					</div>
 				</div>
 			</div>
--- pages/homes/index.js
+++ pages/homes/index.js
@@ -1,71 +1,70 @@
-import React, { useState, useEffect } from "react";
-import Image from "next/image";
-import { useRouter } from "next/router";
-const { format } = require("date-fns");
-import Footer from "../../components/Footer";
-import SearchHeader from "../../components/SearchHeader";
-import HomeDetail from "../../components/HomeDetail";
+import React, { useState, useEffect } from 'react';
+import Image from 'next/image';
+import { useRouter } from 'next/router';
+import { format, differenceInDays } from 'date-fns';
+import Footer from '../../components/Footer';
+import SearchHeader from '../../components/SearchHeader';
+import HomeDetail from '../../components/HomeDetail';
 
 function HomeListings({ data }) {
-  const router = useRouter();
-  const { startDate, endDate, location, totalGuests } = router.query;
+	const router = useRouter();
+	const { startDate, endDate, location, totalGuests } = router.query;
 
-  //   const formattedStartDate = startDate.split(",")[0];
-  //   const formattedEndDate = endDate.split(",")[0];
-  const formattedLocation = location.split(",")[0];
-  const headerStart = new Date(startDate);
-  const start = format(headerStart, "MMM d");
-  const headerEnd = new Date(endDate);
-  const sameEnd = format(headerEnd, "MMM d");
-  //   let end = format(headerEnd, "d");
+	const formattedLocation = location.split(',')[0];
+	const headerStart = new Date(startDate);
+	const start = format(headerStart, 'MMM d');
+	const headerEnd = new Date(endDate);
+	const sameEnd = format(headerEnd, 'MMM d');
 
-  const end =
-    start.split(" ")[0] == sameEnd.split(" ")[0]
-      ? format(headerEnd, "d")
-      : format(headerEnd, "MMM d");
+	const dayCount = differenceInDays(headerEnd, headerStart);
 
-  console.log(start.split(" ")[0]);
-  console.log(end.split(" ")[0]);
+	const end =
+		start.split(' ')[0] == sameEnd.split(' ')[0]
+			? format(headerEnd, 'd')
+			: format(headerEnd, 'MMM d');
 
-  return (
-    <div className="box-border">
-      <SearchHeader
-        formattedLocation={formattedLocation}
-        start={start}
-        end={end}
-        guestCount={totalGuests}
-      />
-      <div className="mx-6 my-24">
-        <section>
-          <div className="text-sm text-gray-900">
-            18 stays · {start}-{end} · {totalGuests}{" "}
-            {totalGuests == 1 ? <span>guest</span> : <span>guests</span>}
-          </div>
-          <div className="text-3xl font-bold">Stays in {formattedLocation}</div>
-          <div>
-            {data.map((item) => {
-              return (
-                <div key={item.id}>
-                  <HomeDetail item={item} />
-                </div>
-              );
-            })}
-          </div>
-        </section>
-        <Footer />
-      </div>
-    </div>
-  );
+	console.log(start.split(' ')[0]);
+	console.log(end.split(' ')[0]);
+
+	return (
+		<div className="box-border">
+			<SearchHeader
+				formattedLocation={formattedLocation}
+				start={start}
+				end={end}
+				guestCount={totalGuests}
+			/>
+			<div className="mx-6 my-24">
+				<section>
+					<div className="text-sm text-gray-900">
+						18 stays · {start}-{end} · {totalGuests}{' '}
+						{totalGuests == 1 ? <span>guest</span> : <span>guests</span>}
+					</div>
+					<div className="text-3xl font-bold">Stays in {formattedLocation}</div>
+					<div>
+						{data.map((item) => {
+							return (
+								<div key={item.id}>
+									<HomeDetail item={item} dayCount={dayCount} />
+								</div>
+							);
+						})}
+					</div>
+				</section>
+				<Footer />
+			</div>
+		</div>
+	);
 }
 
 export default HomeListings;
 export async function getServerSideProps(context) {
-  const response = await fetch("http://localhost:3001/api/homes");
-  const data = await response.json();
-  console.log(data);
-  return {
-    props: { data },
-  };
+	const response = await fetch('http://localhost:3001/api/homes');
+	const data = await response.json();
+	console.log(data);
+	return {
+		props: { data },
+	};
 }
 
 // export async function getStaticProps() {
