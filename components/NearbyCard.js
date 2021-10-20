662e742ca2931b696625578ab7e8473628b8f113
coscoaj1 <coscoanthony1@gmail.com>
2021-10-20 19:49:30 -0400

    margins

--- components/NearbyCard.js
+++ components/NearbyCard.js
@@ -1,22 +1,22 @@
-import React from 'react';
-import Image from 'next/image';
+import React from "react";
+import Image from "next/image";
 
 export default function NearbyCard({ card }) {
-	return (
-		<div className=" flex flex-row  my-5  " key={card.name}>
-			<Image
-				className="rounded-md"
-				src={card.img}
-				alt=""
-				width={65}
-				height={65}
-			/>
-			<div className="flex items-center text-base ml-3">
-				<ul>
-					<li className="font-medium">{card.name}</li>
-					<li>{card.drive}</li>
-				</ul>
-			</div>
-		</div>
-	);
+  return (
+    <div className=" flex flex-row my-5" key={card.name}>
+      <Image
+        className="rounded-md"
+        src={card.img}
+        alt=""
+        width={65}
+        height={65}
+      />
+      <div className="flex items-center text-base ml-3">
+        <ul>
+          <li className="font-medium">{card.name}</li>
+          <li>{card.drive}</li>
+        </ul>
+      </div>
+    </div>
+  );
 }
