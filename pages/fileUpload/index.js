f1d018ce076608d07ca3998174e19318b9084c89
Anthony Cosco <coscoanthony1@gmail.com>
2021-10-21 19:37:13 -0400

    this code works again

--- pages/fileUpload/index.js
+++ pages/fileUpload/index.js
@@ -1,53 +1,53 @@
-import React, { useState } from "react";
-import axios from "axios";
+import React, { useState } from 'react';
+import axios from 'axios';
 
-const baseUrl = "http://localhost:3001/api/homes";
+const baseUrl = 'http://localhost:3001/api/homes';
 
 async function postHome({ image, description }) {
-  const formData = new FormData();
-  formData.append("image", image);
-  formData.append("description", description);
+	const formData = new FormData();
+	formData.append('image', image);
+	formData.append('description', description);
 
-  const result = await axios.post(`${baseUrl}/images`, formData, {
-    headers: { "Content-Type": "multipart/form-data" },
-  });
-  return result.data;
+	const result = await axios.post(`${baseUrl}/homeImage`, formData, {
+		headers: { 'Content-Type': 'multipart/form-data' },
+	});
+	return result.data;
 }
 
 export default function HomeForm() {
-  const [description, setDescription] = useState("");
-  const [file, setFile] = useState();
+	const [description, setDescription] = useState('');
+	const [file, setFile] = useState();
 
-  const handleSubmit = async (event) => {
-    event.preventDefault();
-    const result = await postHome({ image: file, description });
-  };
+	const handleSubmit = async (event) => {
+		event.preventDefault();
+		const result = await postHome({ image: file, description });
+	};
 
-  const fileSelected = (event) => {
-    const file = event.target.files[0];
-    setFile(file);
-  };
+	const fileSelected = (event) => {
+		const file = event.target.files[0];
+		setFile(file);
+	};
 
-  return (
-    <div>
-      <form onSubmit={handleSubmit}>
-        <input onChange={fileSelected} type="file" accept="image/*"></input>
-        <input
-          value={description}
-          type="text"
-          onChange={(e) => setDescription(e.target.value)}
-        ></input>
+	return (
+		<div>
+			<form onSubmit={handleSubmit}>
+				<input onChange={fileSelected} type="file" accept="image/*"></input>
+				<input
+					value={description}
+					type="text"
+					onChange={(e) => setDescription(e.target.value)}
+				></input>
 
-        <button
-          onClick={() => {
-            window.alert("file uploaded");
-          }}
-          className="px-4 py-2 border border-black rounded-lg"
-          type="submit"
-        >
-          Submit
-        </button>
-      </form>
-    </div>
-  );
+				<button
+					onClick={() => {
+						window.alert('file uploaded');
+					}}
+					className="px-4 py-2 border border-black rounded-lg"
+					type="submit"
+				>
+					Submit
+				</button>
+			</form>
+		</div>
+	);
 }
