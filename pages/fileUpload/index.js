5fbc9d1a901e4089f12d7ea39581f433191d5426
Anthony Cosco <coscoanthony1@gmail.com>
2021-10-21 20:32:06 -0400

    Formik working

--- pages/fileUpload/index.js
+++ pages/fileUpload/index.js
@@ -1,53 +1,71 @@
 import React, { useState } from 'react';
 import axios from 'axios';
+import { Formik, Form, Field } from 'formik';
 
 const baseUrl = 'http://localhost:3001/api/homes';
 
-async function postHome({ image, description }) {
-	const formData = new FormData();
-	formData.append('image', image);
-	formData.append('description', description);
+// async function postHome({ image, description }) {
+// 	const formData = new FormData();
+// 	formData.append('image', image);
+// 	formData.append('description', description);
 
-	const result = await axios.post(`${baseUrl}/homeImage`, formData, {
-		headers: { 'Content-Type': 'multipart/form-data' },
-	});
-	return result.data;
-}
+// 	const result = await axios.post(`${baseUrl}/homeImage`, formData, {
+// 		headers: { 'Content-Type': 'multipart/form-data' },
+// 	});
+// 	return result.data;
+// }
 
 export default function HomeForm() {
-	const [description, setDescription] = useState('');
-	const [file, setFile] = useState();
+	// const [description, setDescription] = useState('');
+	// const [file, setFile] = useState();
 
-	const handleSubmit = async (event) => {
-		event.preventDefault();
-		const result = await postHome({ image: file, description });
-	};
+	// const handleSubmit = async (event) => {
+	// 	event.preventDefault();
+	// 	const result = await postHome({ image: file, description });
+	// };
 
-	const fileSelected = (event) => {
-		const file = event.target.files[0];
-		setFile(file);
-	};
+	// const fileSelected = (event) => {
+	// 	const file = event.target.files[0];
+	// 	setFile(file);
+	// };
 
 	return (
 		<div>
-			<form onSubmit={handleSubmit}>
-				<input onChange={fileSelected} type="file" accept="image/*"></input>
-				<input
-					value={description}
-					type="text"
-					onChange={(e) => setDescription(e.target.value)}
-				></input>
+			<Formik
+				initialValues={{ image: '', description: '' }}
+				onSubmit={async (values) => {
+					const formData = new FormData();
+					formData.append('image', values.image);
+					formData.append('description', values.description);
 
-				<button
-					onClick={() => {
-						window.alert('file uploaded');
-					}}
-					className="px-4 py-2 border border-black rounded-lg"
-					type="submit"
-				>
-					Submit
-				</button>
-			</form>
+					const result = await axios.post(`${baseUrl}/homeImage`, formData, {
+						headers: { 'Content-Type': 'multipart/form-data' },
+					});
+					return result.data;
+				}}
+			>
+				{(props) => (
+					<Form>
+						<input
+							onChange={(event) =>
+								props.setFieldValue('image', event.target.files[0])
+							}
+							type="file"
+							accept="image/*"
+						></input>
+						<Field type="text" id="description" name="description"></Field>
+						<button
+							onClick={() => {
+								window.alert('file uploaded');
+							}}
+							className="px-4 py-2 border border-black rounded-lg"
+							type="submit"
+						>
+							Submit
+						</button>
+					</Form>
+				)}
+			</Formik>
 		</div>
 	);
 }
