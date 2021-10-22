bab6e673e75970df68172d3907845f89c6a1d8c6
Anthony Cosco <coscoanthony1@gmail.com>
2021-10-22 06:38:30 -0400

    yup not working

--- pages/fileUpload/index.js
+++ pages/fileUpload/index.js
@@ -1,8 +1,85 @@
-import React, { useState } from 'react';
+import React from 'react';
 import axios from 'axios';
 import { Formik, Form, Field } from 'formik';
+import * as Yup from 'yup';
 
 const baseUrl = 'http://localhost:3001/api/homes';
+
+export default function HomeForm() {
+	const validationSchema = Yup.object().shape({
+		image: Yup.mixed().required('Image required'),
+		description: Yup.string()
+			.required('Description required')
+			.min(8, 'Must be at least 8 characters')
+			.matches(/^[a-zA-Z0-9]+$/, 'Cannot contain special characters or spaces'),
+		title: Yup.string()
+			.required('Title required.')
+			.min(6, 'Must be at least 6 characters')
+			.matches(/^[a-zA-Z0-9]+$/, 'Cannot contain special characters or spaces'),
+		price: Yup.number().required('Price required'),
+	});
+
+	return (
+		<div>
+			<Formik
+				initialValues={{ image: '', description: '', title: '', price: '' }}
+				validationSchema={validationSchema}
+				onSubmit={async (values) => {
+					const formData = new FormData();
+					formData.append('image', values.image);
+					formData.append('description', values.description);
+					formData.append('title', values.title);
+					formData.append('price', values.price);
+					console.log(values);
+
+					const result = await axios.post(`${baseUrl}/homeImage`, formData, {
+						headers: { 'Content-Type': 'multipart/form-data' },
+					});
+					return result.data;
+				}}
+			>
+				{(props) => (
+					<Form className="flex flex-col items-center justify-center">
+						<input
+							onChange={(event) =>
+								props.setFieldValue('image', event.target.files[0])
+							}
+							type="file"
+							accept="image/*"
+						></input>
+						<label htmlFor="title">description</label>
+						<Field
+							className="border border-gray-700 py-1 px-2"
+							type="text"
+							id="description"
+							name="description"
+						></Field>
+						<label htmlFor="title">title</label>
+						<Field
+							className="border border-gray-700 py-1 px-2"
+							type="text"
+							id="title"
+							name="title"
+						></Field>
+						<label htmlFor="price">price</label>
+						<Field
+							className="border border-gray-700 py-1 px-2"
+							type="text"
+							id="price"
+							name="price"
+						></Field>
+						<button
+							className="transform active:scale-90 duration-150 px-4 py-2 border border-black rounded-lg"
+							type="submit"
+						>
+							Submit
+						</button>
+					</Form>
+				)}
+			</Formik>
+		</div>
+	);
+}
 
 // async function postHome({ image, description }) {
 // 	const formData = new FormData();
@@ -14,58 +91,15 @@ const baseUrl = 'http://localhost:3001/api/homes';
 // 	});
 // 	return result.data;
 // }
+// const [description, setDescription] = useState('');
+// const [file, setFile] = useState();
 
-export default function HomeForm() {
-	// const [description, setDescription] = useState('');
-	// const [file, setFile] = useState();
+// const handleSubmit = async (event) => {
+// 	event.preventDefault();
+// 	const result = await postHome({ image: file, description });
+// };
 
-	// const handleSubmit = async (event) => {
-	// 	event.preventDefault();
-	// 	const result = await postHome({ image: file, description });
-	// };
-
-	// const fileSelected = (event) => {
-	// 	const file = event.target.files[0];
-	// 	setFile(file);
-	// };
-
-	return (
-		<div>
-			<Formik
-				initialValues={{ image: '', description: '' }}
-				onSubmit={async (values) => {
-					const formData = new FormData();
-					formData.append('image', values.image);
-					formData.append('description', values.description);
-
-					const result = await axios.post(`${baseUrl}/homeImage`, formData, {
-						headers: { 'Content-Type': 'multipart/form-data' },
-					});
-					return result.data;
-				}}
-			>
-				{(props) => (
-					<Form>
-						<input
-							onChange={(event) =>
-								props.setFieldValue('image', event.target.files[0])
-							}
-							type="file"
-							accept="image/*"
-						></input>
-						<Field type="text" id="description" name="description"></Field>
-						<button
-							onClick={() => {
-								window.alert('file uploaded');
-							}}
-							className="px-4 py-2 border border-black rounded-lg"
-							type="submit"
-						>
-							Submit
-						</button>
-					</Form>
-				)}
-			</Formik>
-		</div>
-	);
-}
+// const fileSelected = (event) => {
+// 	const file = event.target.files[0];
+// 	setFile(file);
+// };
