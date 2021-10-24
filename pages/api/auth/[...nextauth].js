a259a0f404070f957f2f3bada34e21a03a8f5e60
Anthony Cosco <coscoanthony1@gmail.com>
2021-10-24 17:14:12 -0400

    email

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,31 +1,31 @@
-import NextAuth from "next-auth";
-import EmailProvider from "next-auth/providers/Email";
-import GithubProvider from "next-auth/providers/Github";
-import { MongoDBAdapter } from "@next-auth/mongodb-adapter";
-import clientPromise from "../../../lib/mongodb";
+import NextAuth from 'next-auth';
+import EmailProvider from 'next-auth/providers/email';
+import GithubProvider from 'next-auth/providers/Github';
+import { MongoDBAdapter } from '@next-auth/mongodb-adapter';
+import clientPromise from '../../../lib/mongodb';
 
 export default async function auth(req, res) {
-  return await NextAuth(req, res, {
-    adapter: MongoDBAdapter({
-      db: (await clientPromise).db("airbnbclone"),
-    }),
-    providers: [
-      GithubProvider({
-        clientId: process.env.GITHUB_ID,
-        clientSecret: process.env.GITHUB_SECRET,
-      }),
-      EmailProvider({
-        server: {
-          host: process.env.EMAIL_SERVER_HOST,
-          port: process.env.EMAIL_SERVER_PORT,
-          auth: {
-            user: process.env.EMAIL_SERVER_USER,
-            pass: process.env.EMAIL_SERVER_PASSWORD,
-          },
-        },
-        from: process.env.EMAIL_FROM,
-        maxAge: 24 * 60 * 60,
-      }),
-    ],
-  });
+	return await NextAuth(req, res, {
+		adapter: MongoDBAdapter({
+			db: (await clientPromise).db('airbnbclone'),
+		}),
+		providers: [
+			GithubProvider({
+				clientId: process.env.GITHUB_ID,
+				clientSecret: process.env.GITHUB_SECRET,
+			}),
+			EmailProvider({
+				server: {
+					host: process.env.EMAIL_SERVER_HOST,
+					port: process.env.EMAIL_SERVER_PORT,
+					auth: {
+						user: process.env.EMAIL_SERVER_USER,
+						pass: process.env.EMAIL_SERVER_PASSWORD,
+					},
+				},
+				from: process.env.EMAIL_FROM,
+				maxAge: 24 * 60 * 60,
+			}),
+		],
+	});
 }
