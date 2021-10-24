f411020bf64d2cfb3e773aab47836d611218b1fc
coscoaj1 <coscoanthony1@gmail.com>
2021-10-24 19:14:50 -0400

    login

--- components/LoginModal.js
+++ components/LoginModal.js
@@ -29,41 +29,33 @@ function LoginModal({ open, onClose, crsfToken }) {
           Welcome to Airbnb
         </Dialog.Description>
         <div className="flex flex-col h-lg w-full ">
-          <Link
-            className="flex flex-col"
-            href="/api/auth/signin/email"
-            passHref
+          <button
+            onClick={(e) => {
+              e.preventDefault();
+              signIn();
+            }}
+            className="w-lg mx-4 my-5 py-3 pl-1 bg-red-500 text-white text-base font-medium rounded-md active:scale-95 transform duration-200"
           >
-            <button
-              onClick={(e) => {
-                e.preventDefault();
-                signIn();
-              }}
-              className="w-lg mx-4 my-5 py-3 pl-1 bg-red-500 text-white text-base font-medium rounded-md active:scale-95 transform duration-200"
-            >
-              Continue with Email
-            </button>
-          </Link>
+            Continue with Email
+          </button>
           <div
             className="border-b border-gray-30 mx-4 mt-3 leading-2
 											 w-lg text-center "
           >
             <span className="px-2 bg-white">or</span>
           </div>
-          <Link href="/api/auth/signin" passHref>
-            <button
-              onClick={(e) => {
-                e.preventDefault();
-                signIn();
-              }}
-              className="flex items-start gap-28 w-lg mx-4 my-5 pl-1 text-base font-medium border border-black rounded-md active:scale-95 transform duration-200"
-            >
-              <div className="flex justify-center items-center h-full mr-2">
-                <GithubIcon width={24} height={24} alt="" />
-              </div>
-              <div className="py-3 text-center">Continue with Github</div>
-            </button>
-          </Link>
+          <button
+            onClick={(e) => {
+              e.preventDefault();
+              signIn();
+            }}
+            className="flex items-start gap-28 w-lg mx-4 my-5 pl-1 text-base font-medium border border-black rounded-md active:scale-95 transform duration-200"
+          >
+            <div className="flex justify-center items-center h-full mr-2">
+              <GithubIcon width={24} height={24} alt="" />
+            </div>
+            <div className="py-3 text-center">Continue with Github</div>
+          </button>
         </div>
       </Dialog>
     </div>
