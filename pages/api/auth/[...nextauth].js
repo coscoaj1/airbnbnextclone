58e4a99a3488ef424ced8ec0af0d117c3294086a
Anthony Cosco <coscoanthony1@gmail.com>
2021-10-24 20:39:54 -0400

    email

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -14,18 +14,18 @@ export default async function auth(req, res) {
 				clientId: process.env.GITHUB_ID,
 				clientSecret: process.env.GITHUB_SECRET,
 			}),
-			// EmailProvider({
-			// 	server: {
-			// 		host: process.env.EMAIL_SERVER_HOST,
-			// 		port: process.env.EMAIL_SERVER_PORT,
-			// 		auth: {
-			// 			user: process.env.EMAIL_SERVER_USER,
-			// 			pass: process.env.EMAIL_SERVER_PASSWORD,
-			// 		},
-			// 	},
-			// 	from: process.env.EMAIL_FROM,
-			// 	maxAge: 24 * 60 * 60,
-			// }),
+			EmailProvider({
+				server: {
+					host: process.env.EMAIL_SERVER_HOST,
+					port: process.env.EMAIL_SERVER_PORT,
+					auth: {
+						user: process.env.EMAIL_SERVER_USER,
+						pass: process.env.EMAIL_SERVER_PASSWORD,
+					},
+				},
+				from: process.env.EMAIL_FROM,
+				maxAge: 24 * 60 * 60,
+			}),
 		],
 	});
 }
