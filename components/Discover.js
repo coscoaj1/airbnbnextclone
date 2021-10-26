3cbfa64a6f7943a8d1382f36ce4ff462e83bba56
Anthony Cosco <coscoanthony1@gmail.com>
2021-10-26 05:11:38 -0400

    responsive

--- components/Discover.js
+++ components/Discover.js
@@ -1,40 +1,40 @@
-import React from "react";
-import DiscoverCard from "./DiscoverCard";
-import goatImg from "../public/images/goatImg.jpg";
-import tabletImg from "../public/images/tabletImg.jpg";
-import kayakImg from "../public/images/kayakImg.jpg";
+import React from 'react';
+import DiscoverCard from './DiscoverCard';
+import goatImg from '../public/images/goatImg.jpg';
+import tabletImg from '../public/images/tabletImg.jpg';
+import kayakImg from '../public/images/kayakImg.jpg';
 
 const discoverList = [
-  {
-    img: goatImg,
-    title: "Experiences",
-    description: "Find unforgettable activities near you.",
-  },
-  {
-    img: tabletImg,
-    title: "Online Experiences",
-    description: "Live, interactive activites led by Hosts.",
-  },
-  {
-    img: kayakImg,
-    title: "Outdoor collection",
-    description: "Experiences that immerse you in nature.",
-  },
+	{
+		img: goatImg,
+		title: 'Experiences',
+		description: 'Find unforgettable activities near you.',
+	},
+	{
+		img: tabletImg,
+		title: 'Online Experiences',
+		description: 'Live, interactive activites led by Hosts.',
+	},
+	{
+		img: kayakImg,
+		title: 'Outdoor collection',
+		description: 'Experiences that immerse you in nature.',
+	},
 ];
 
 function Discover() {
-  return (
-    <div className="my-8 h-auto w-full">
-      <h2 className="text-xl sm:text-2xl md:text-4xl mb-4 font-bold">
-        Discover things to do
-      </h2>
-      <div className="flex flex-row gap-4 items-center justify-center ">
-        {discoverList.map((card) => (
-          <DiscoverCard key={card.title} card={card} />
-        ))}
-      </div>
-    </div>
-  );
+	return (
+		<div className="my-8 h-auto w-full">
+			<h2 className="text-xl sm:text-2xl md:text-4xl mb-4 font-bold text-center sm:text-left">
+				Discover things to do
+			</h2>
+			<div className="flex gap-1 items-center justify-center ">
+				{discoverList.map((card) => (
+					<DiscoverCard key={card.title} card={card} />
+				))}
+			</div>
+		</div>
+	);
 }
 
 export default Discover;
