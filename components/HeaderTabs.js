a07200536ccde66de6654969bd2266f64ef80243
coscoaj1 <coscoanthony1@gmail.com>
2021-11-15 17:38:14 -0500

    fixed head tab hover styling

--- components/HeaderTabs.js
+++ components/HeaderTabs.js
@@ -1,24 +1,23 @@
-import React, {useState} from 'react';
-
+import React, { useState } from "react";
 
 function HeaderTabs({ navbar }) {
-	const [activeTab, setActiveTab] = useState()
-	return (
-		<div className={navbar ? 'navsearch-active' : 'navsearch'}>
-			<button className="mx-8 relative ">
-				<span>Places to stay</span>
-				<div className="border-b-2 border-white mt-1 w-4 left-10 absolute"></div>
-			</button>
-			<button className="relative group mx-8 hover:text-gray-300">
-				<span>Experiences</span>
-				<div className="transition delay-50 duration-200 ease-in border-b-2 group-hover:border-white border-green-900 mt-1 w-1 left-10 absolute"></div>
-				</button>
-			<button className="relative group mx-8 hover:text-gray-300">
-				<span>Online Experiences</span>
-				<div className="transition delay-50 duration-200 ease-in border-b-2 group-hover:border-white border-green-900 mt-1 w-1 left-16 absolute"></div>
-				</button>
-		</div>
-	);
+  const [activeTab, setActiveTab] = useState();
+  return (
+    <div className={navbar ? "navsearch-active" : "navsearch"}>
+      <button className="relative mx-8 group hover:text-gray-300">
+        <span>Places to stay</span>
+        <div className="absolute w-4 mt-1 transition duration-200 ease-in border-b-2 border-green-900 delay-50 group-hover:border-white left-10"></div>
+      </button>
+      <button className="relative mx-8 group hover:text-gray-300">
+        <span>Experiences</span>
+        <div className="absolute w-1 mt-1 transition duration-200 ease-in border-b-2 border-green-900 delay-50 group-hover:border-white left-10"></div>
+      </button>
+      <button className="relative mx-8 group hover:text-gray-300">
+        <span>Online Experiences</span>
+        <div className="absolute w-1 mt-1 transition duration-200 ease-in border-b-2 border-green-900 delay-50 group-hover:border-white left-16"></div>
+      </button>
+    </div>
+  );
 }
 
 export default HeaderTabs;
