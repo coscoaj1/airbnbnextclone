086b965220b730544174678fa64e0f04aadf811c
coscoaj1 <coscoanthony1@gmail.com>
2021-11-18 20:51:57 -0500

    log in button/user drop down resize

--- components/LoginModal.js
+++ components/LoginModal.js
@@ -1,77 +1,74 @@
-import React from 'react';
-import { getCrsfToken } from 'next-auth/react';
-import { signIn } from 'next-auth/react';
-import { Dialog } from '@headlessui/react';
-import CancelIcon from '../public/images/x-svgrepo-com (1).svg';
-import TwitterIcon from '../public/images/icons8-twitter-30.png';
-import Image from 'next/image';
+import React from "react";
+import { getCrsfToken } from "next-auth/react";
+import { signIn } from "next-auth/react";
+import { Dialog } from "@headlessui/react";
+import CancelIcon from "../public/images/x-svgrepo-com (1).svg";
+import TwitterIcon from "../public/images/icons8-twitter-30.png";
+import Image from "next/image";
 
 function LoginModal({ open, onClose, crsfToken }) {
-	return (
-		<div className={open ? 'z-10 fixed inset-0 bg-black opacity-40' : ''}>
-			<Dialog
-				className="bg-white mx-auto my-auto text-black z-30 fixed inset-0 max-w-lg h-xl rounded-xl "
-				open={open}
-				onClose={onClose}
-			>
-				<Dialog.Overlay />
-				<Dialog.Title className="flex w-full items-center justify-between ">
-					<button
-						onClick={onClose}
-						className="text-left ml-4 rounded-full hover:bg-gray-200 w-6 h-6 border-none"
-					>
-						<CancelIcon className="pl-1" width="20" height="20" alt="" />
-					</button>
-					<div className="font-bold mr-48 py-4">Log in or sign up</div>
-				</Dialog.Title>
-				<div className="border-b border-gray-200 mt-1"></div>
-				<Dialog.Description className="text-2xl font-medium py-4 ml-4 overflow-y-auto">
-					Welcome to Airbnb
-				</Dialog.Description>
-				<div className="flex flex-col h-lg w-full ">
-					<button
-						onClick={(e) => {
-							e.preventDefault();
-							signIn('email', {
-								callbackUrl: 'https://airbnbnextclone.vercel.app',
-							});
-						}}
-						className="w-lg mx-4 my-5 py-3 pl-1 bg-red-500 text-white text-base font-medium rounded-md active:scale-95 transform duration-200"
-					>
-						Continue with Email
-					</button>
+  return (
+    <div className={open ? "z-10 fixed inset-0 bg-black opacity-40" : ""}>
+      <Dialog
+        className="fixed inset-0 z-30 max-w-lg mx-auto my-auto text-black bg-white h-lg rounded-xl "
+        open={open}
+        onClose={onClose}
+      >
+        <Dialog.Overlay />
+        <Dialog.Title className="flex items-center justify-between w-full ">
+          <button
+            onClick={onClose}
+            className="w-6 h-6 ml-4 text-left border-none rounded-full hover:bg-gray-200"
+          >
+            <CancelIcon className="pl-1" width="20" height="20" alt="" />
+          </button>
+          <div className="py-4 mr-48 font-bold">Log in or sign up</div>
+        </Dialog.Title>
+        <div className="mt-1 border-b border-gray-200"></div>
+        <Dialog.Description className="py-4 ml-4 overflow-y-auto text-2xl font-medium">
+          Welcome to Airbnb
+        </Dialog.Description>
+        <div className="flex flex-col w-full h-lg ">
+          <button
+            onClick={(e) => {
+              e.preventDefault();
+              signIn("email", {
+                callbackUrl: "https://airbnbnextclone.vercel.app",
+              });
+            }}
+            className="py-3 pl-1 mx-4 my-5 text-base font-medium text-white duration-200 transform bg-red-500 rounded-md w-lg active:scale-95"
+          >
+            Continue with Email
+          </button>
 
-					<div
-						className="border-b border-gray-30 mx-4 mt-3 leading-2
-											 w-lg text-center "
-					>
-						<span className="px-2 bg-white">or</span>
-					</div>
-					<button
-						onClick={(e) => {
-							e.preventDefault();
-							signIn('twitter', {
-								callbackUrl: 'https://airbnbnextclone.vercel.app',
-							});
-						}}
-						className="flex items-start gap-28 w-lg mx-4 my-5 pl-1 text-base font-medium border border-black rounded-md active:scale-95 transform duration-200"
-					>
-						<div className="flex justify-center items-center h-full mr-2">
-							<Image src={TwitterIcon} width={32} height={32} alt="" />
-						</div>
-						<div className="py-3 text-center">Continue with Twitter</div>
-					</button>
-				</div>
-			</Dialog>
-		</div>
-	);
+          <div className="mx-4 mt-3 text-center border-b border-gray-30 leading-2 w-lg ">
+            <span className="px-2 bg-white">or</span>
+          </div>
+          <button
+            onClick={(e) => {
+              e.preventDefault();
+              signIn("twitter", {
+                callbackUrl: "https://airbnbnextclone.vercel.app",
+              });
+            }}
+            className="flex items-start pl-1 mx-4 mt-8 text-base font-medium duration-200 transform border border-black rounded-md gap-28 w-lg active:scale-95"
+          >
+            <div className="flex items-center justify-center h-full mr-2">
+              <Image src={TwitterIcon} width={32} height={32} alt="" />
+            </div>
+            <div className="py-3 text-center">Continue with Twitter</div>
+          </button>
+        </div>
+      </Dialog>
+    </div>
+  );
 }
 
 export default LoginModal;
 
 export async function getServerSideProps(context) {
-	const csrfToken = await getCsrfToken(context);
-	return {
-		props: { csrfToken },
-	};
+  const csrfToken = await getCsrfToken(context);
+  return {
+    props: { csrfToken },
+  };
 }
--- components/UserDropDown.js
+++ components/UserDropDown.js
@@ -1,234 +1,232 @@
-import React, { useState } from 'react';
-import { Menu } from '@headlessui/react';
-import Image from 'next/image';
-import Link from 'next/link';
-import { signOut, useSession } from 'next-auth/react';
-import userIcon from '../public/images/profile-user.png';
-import menuIcon from '../public/images/icons8-menu-24.png';
-import LoginModal from './LoginModal';
-import loggedInIcon from '../public/images/user_pic-50x50.png';
+import React, { useState } from "react";
+import { Menu } from "@headlessui/react";
+import Image from "next/image";
+import Link from "next/link";
+import { signOut, useSession } from "next-auth/react";
+import userIcon from "../public/images/profile-user.png";
+import menuIcon from "../public/images/icons8-menu-24.png";
+import LoginModal from "./LoginModal";
+import loggedInIcon from "../public/images/user_pic-50x50.png";
 
 export default function UserDropDown({ navbar }) {
-	const [showModal, setShowModal] = useState(false);
-	const [isOpen, setIsOpen] = useState(true);
-	const { data: session, status } = useSession();
-	const loading = status === 'loading';
+  const [showModal, setShowModal] = useState(false);
+  const [isOpen, setIsOpen] = useState(true);
+  const { data: session, status } = useSession();
+  const loading = status === "loading";
 
-	const onClick = () => {
-		setShowModal(true);
-		setIsOpen(true);
-	};
+  const onClick = () => {
+    setShowModal(true);
+    setIsOpen(true);
+  };
 
-	const handleClose = (e) => {
-		setIsOpen(false);
-	};
+  const handleClose = (e) => {
+    setIsOpen(false);
+  };
 
-	return (
-		<>
-			<div className="-mr-6">
-				<Menu>
-					<Menu.Button>
-						<div className="flex flex-row items-center mr-6 bg-white rounded-full border transition-shadow hover:shadow-md">
-							<div className="w-5 h-auto mt-2 mb-1 ml-2 z-10">
-								<Image src={menuIcon} width={16} height={18} alt="" />
-							</div>
-							<div className="w-7 mx-2 mt-1.5">
-								{session ? (
-									<Image
-										className="h-8 rounded-full"
-										src={loggedInIcon}
-										alt=""
-									/>
-								) : (
-									<Image src={userIcon} alt="" />
-								)}
-							</div>
-						</div>
-					</Menu.Button>
-					<Menu.Items className="z-10 absolute right-0 w-64 h-auto mt-2 origin-top-right bg-white text-black divide-y divide-gray-100 rounded-xl shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
-						<div className="flex flex-col px-1 py-1 border-b border-gray-200 text-sm">
-							{!loading && !session && (
-								<Menu.Item>
-									{({ active }) => (
-										<li
-											as=""
-											className={`${
-												active ? ' bg-gray-100' : 'bg-white'
-											} w-full font-medium px-3 py-2.5 mx-0 rounded-lg list-none`}
-										>
-											<button
-												className="w-full font-medium text-left"
-												onClick={onClick}
-											>
-												Sign up
-											</button>
-										</li>
-									)}
-								</Menu.Item>
-							)}
-							{session && (
-								<Menu.Item>
-									{({ active }) => (
-										<li
-											as=""
-											className={`${
-												active ? ' bg-gray-100' : 'bg-white'
-											} w-full font-medium px-3 py-2.5 mx-0 rounded-lg list-none`}
-										>
-											<button className="w-full font-medium text-left">
-												Messages
-											</button>
-										</li>
-									)}
-								</Menu.Item>
-							)}
-							{session && (
-								<Menu.Item>
-									{({ active }) => (
-										<li
-											as=""
-											className={`${
-												active ? ' bg-gray-100' : 'bg-white'
-											} w-full font-medium px-3 py-2.5 mx-0 rounded-lg list-none`}
-										>
-											<button className="w-full font-medium text-left">
-												Trips
-											</button>
-										</li>
-									)}
-								</Menu.Item>
-							)}
-							{session && (
-								<Menu.Item>
-									{({ active }) => (
-										<li
-											as=""
-											className={`${
-												active ? ' bg-gray-100' : 'bg-white'
-											} w-full font-medium px-3 py-2.5 mx-0 rounded-lg list-none`}
-										>
-											<button className="w-full font-medium text-left">
-												Wishlists
-											</button>
-										</li>
-									)}
-								</Menu.Item>
-							)}
+  return (
+    <>
+      <div className="-mr-6">
+        <Menu>
+          <Menu.Button>
+            <div className="flex flex-row items-center mr-6 transition-shadow bg-white border rounded-full hover:shadow-md">
+              <div className="z-10 w-5 h-auto mt-2 mb-1 ml-2">
+                <Image src={menuIcon} width={16} height={18} alt="" />
+              </div>
+              <div className="w-7 mx-2 mt-1.5">
+                {session ? (
+                  <Image
+                    className="h-8 rounded-full"
+                    src={loggedInIcon}
+                    alt=""
+                  />
+                ) : (
+                  <Image src={userIcon} alt="" />
+                )}
+              </div>
+            </div>
+          </Menu.Button>
+          <Menu.Items className="absolute right-0 z-10 w-64 h-auto mt-2 text-black origin-top-right bg-white divide-y divide-gray-100 shadow-lg rounded-xl ring-1 ring-black ring-opacity-5 focus:outline-none">
+            <div className="flex flex-col px-1 py-1 text-sm border-b border-gray-200">
+              {!loading && !session && (
+                <Menu.Item>
+                  {({ active }) => (
+                    <li
+                      as=""
+                      className={`${
+                        active ? " bg-gray-100" : "bg-white"
+                      } w-full font-medium px-3 py-2.5 mx-0 rounded-lg list-none`}
+                    >
+                      <button
+                        className="w-full font-medium text-left"
+                        onClick={onClick}
+                      >
+                        Sign up
+                      </button>
+                    </li>
+                  )}
+                </Menu.Item>
+              )}
+              {session && (
+                <Menu.Item>
+                  {({ active }) => (
+                    <li
+                      as=""
+                      className={`${
+                        active ? " bg-gray-100" : "bg-white"
+                      } w-full font-medium px-3 py-2.5 mx-0 rounded-lg list-none`}
+                    >
+                      <button className="w-full font-medium text-left">
+                        Messages
+                      </button>
+                    </li>
+                  )}
+                </Menu.Item>
+              )}
+              {session && (
+                <Menu.Item>
+                  {({ active }) => (
+                    <li
+                      as=""
+                      className={`${
+                        active ? " bg-gray-100" : "bg-white"
+                      } w-full font-medium px-3 py-2.5 mx-0 rounded-lg list-none`}
+                    >
+                      <button className="w-full font-medium text-left">
+                        Trips
+                      </button>
+                    </li>
+                  )}
+                </Menu.Item>
+              )}
+              {session && (
+                <Menu.Item>
+                  {({ active }) => (
+                    <li
+                      as=""
+                      className={`${
+                        active ? " bg-gray-100" : "bg-white"
+                      } w-full font-medium px-3 py-2.5 mx-0 rounded-lg list-none`}
+                    >
+                      <button className="w-full font-medium text-left">
+                        Wishlists
+                      </button>
+                    </li>
+                  )}
+                </Menu.Item>
+              )}
 
-							{!session && !loading && (
-								<Menu.Item>
-									{({ active }) => (
-										<li
-											as=""
-											className={`${
-												active ? 'bg-gray-100' : 'bg-white'
-											} w-full px-3 py-2.5 mx-0 rounded-lg list-none`}
-											href="#"
-										>
-											<Link href="#">
-												<a className="w-full text-left" onClick={onClick}>
-													Log in
-												</a>
-											</Link>
-										</li>
-									)}
-								</Menu.Item>
-							)}
-						</div>
-						<div className="flex flex-col px-1 py-1 text-sm">
-							<Menu.Item>
-								{({ active }) => (
-									<Link to="/fileUpload" href="/fileUpload">
-										<a
-											as=""
-											className={`${
-												active ? 'bg-gray-100' : 'bg-white'
-											} w-full px-3 py-2.5 mx-0 rounded-lg`}
-											href="#"
-										>
-											Host your home
-										</a>
-									</Link>
-								)}
-							</Menu.Item>
-							<Menu.Item>
-								{({ active }) => (
-									<a
-										as=""
-										className={`${
-											active ? 'bg-gray-100' : 'bg-white'
-										} w-full px-3 py-2.5 mx-0 rounded-lg`}
-										href="#"
-									>
-										Host an experience
-									</a>
-								)}
-							</Menu.Item>
-							{session && (
-								<div className="flex flex-col px-1 py-1 border-b border-gray-200 text-sm">
-									<Menu.Item>
-										{({ active }) => (
-											<a
-												as=""
-												className={`${
-													active ? 'bg-gray-100' : 'bg-white'
-												} w-full px-2 py-2.5 mx-0 rounded-lg`}
-												href="#"
-											>
-												Account
-											</a>
-										)}
-									</Menu.Item>
-								</div>
-							)}
-							<Menu.Item>
-								{({ active }) => (
-									<a
-										as=""
-										className={`${
-											active ? 'bg-gray-100' : 'bg-white'
-										} w-full px-3 py-2.5 mx-0 rounded-lg`}
-										href="#"
-									>
-										Help
-									</a>
-								)}
-							</Menu.Item>
-							{session && (
-								<Menu.Item>
-									{({ active }) => (
-										<li
-											as=""
-											className={`${
-												active ? ' bg-gray-100' : 'bg-white'
-											} w-full font-medium px-3 py-2.5 mx-0 rounded-lg list-none`}
-										>
-											<button
-												type="reset"
-												onClick={(e) => {
-													e.preventDefault();
-													signOut('github');
-												}}
-												className="w-full text-left"
-											>
-												Sign out
-											</button>
-										</li>
-									)}
-								</Menu.Item>
-							)}
-						</div>
-					</Menu.Items>
-				</Menu>
-			</div>
-			{showModal && (
-				<LoginModal
-					open={isOpen}
-					onClose={(e) => {
-						handleClose(e);
-					}}
-				/>
-			)}
-		</>
-	);
+              {!session && !loading && (
+                <Menu.Item>
+                  {({ active }) => (
+                    <li
+                      as=""
+                      className={`${
+                        active ? "bg-gray-100" : "bg-white"
+                      } w-full px-3 py-2.5 mx-0 rounded-lg list-none`}
+                      href="#"
+                    >
+                      <button className="w-full text-left" onClick={onClick}>
+                        Log in
+                      </button>
+                    </li>
+                  )}
+                </Menu.Item>
+              )}
+            </div>
+            <div className="flex flex-col px-1 py-1 text-sm">
+              <Menu.Item>
+                {({ active }) => (
+                  <Link to="/fileUpload" href="/fileUpload">
+                    <a
+                      as=""
+                      className={`${
+                        active ? "bg-gray-100" : "bg-white"
+                      } w-full px-3 py-2.5 mx-0 rounded-lg`}
+                      href="#"
+                    >
+                      Host your home
+                    </a>
+                  </Link>
+                )}
+              </Menu.Item>
+              <Menu.Item>
+                {({ active }) => (
+                  <a
+                    as=""
+                    className={`${
+                      active ? "bg-gray-100" : "bg-white"
+                    } w-full px-3 py-2.5 mx-0 rounded-lg`}
+                    href="#"
+                  >
+                    Host an experience
+                  </a>
+                )}
+              </Menu.Item>
+              {session && (
+                <div className="flex flex-col px-1 py-1 text-sm border-b border-gray-200">
+                  <Menu.Item>
+                    {({ active }) => (
+                      <a
+                        as=""
+                        className={`${
+                          active ? "bg-gray-100" : "bg-white"
+                        } w-full px-2 py-2.5 mx-0 rounded-lg`}
+                        href="#"
+                      >
+                        Account
+                      </a>
+                    )}
+                  </Menu.Item>
+                </div>
+              )}
+              <Menu.Item>
+                {({ active }) => (
+                  <a
+                    as=""
+                    className={`${
+                      active ? "bg-gray-100" : "bg-white"
+                    } w-full px-3 py-2.5 mx-0 rounded-lg`}
+                    href="#"
+                  >
+                    Help
+                  </a>
+                )}
+              </Menu.Item>
+              {session && (
+                <Menu.Item>
+                  {({ active }) => (
+                    <li
+                      as=""
+                      className={`${
+                        active ? " bg-gray-100" : "bg-white"
+                      } w-full font-medium px-3 py-2.5 mx-0 rounded-lg list-none`}
+                    >
+                      <button
+                        type="reset"
+                        onClick={(e) => {
+                          e.preventDefault();
+                          signOut("github");
+                        }}
+                        className="w-full text-left"
+                      >
+                        Sign out
+                      </button>
+                    </li>
+                  )}
+                </Menu.Item>
+              )}
+            </div>
+          </Menu.Items>
+        </Menu>
+      </div>
+      {showModal && (
+        <LoginModal
+          open={isOpen}
+          onClose={(e) => {
+            handleClose(e);
+          }}
+        />
+      )}
+    </>
+  );
 }
