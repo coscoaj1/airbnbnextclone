b9b34c6100a8c73844f633e6715b1f3e9fe327cf
coscoaj1 <coscoanthony1@gmail.com>
2021-11-18 20:43:44 -0500

    logo header host button

--- components/LogoHeader.js
+++ components/LogoHeader.js
@@ -6,11 +6,12 @@ import searchIcon from "../public/images/search-12-32.png";
 import globe from "../public/images/icons8-globe-50.png";
 import blackGlobe from "../public/images/icons8-globe-24.png";
 import UserDropDown from "./UserDropDown";
+import Link from "next/link";
 
 function LogoHeader({ navbar }) {
   return (
     <div className="relative flex flex-row mx-24">
-      <button className="flex-5 flex flex-row items-center">
+      <button className="flex flex-row items-center flex-5">
         <div className={navbar ? "mt-2" : "flex "}>
           <Image
             src={navbar ? redLogo : logo}
@@ -37,8 +38,8 @@ function LogoHeader({ navbar }) {
               : "hidden"
           }
         >
-          <span className="font-medium pl-2">Start your search</span>
-          <span className="bg-airbnb relative rounded-full w-7 h-7 ml-20">
+          <span className="pl-2 font-medium">Start your search</span>
+          <span className="relative ml-20 rounded-full bg-airbnb w-7 h-7">
             <div className="relative w-3 top-1 left-2">
               <Image src={searchIcon} alt="search icon" />
             </div>
@@ -46,15 +47,17 @@ function LogoHeader({ navbar }) {
         </button>
       </div>
       <div className="flex items-center justify-end flex-5">
-        <button
-          className={
-            navbar
-              ? "py-2 px-3 text-sm font-medium hover:bg-gray-100 rounded-2xl"
-              : "py-2 px-3 text-sm font-medium hover:bg-gray-50 hover:bg-opacity-20  rounded-2xl"
-          }
-        >
-          Become a host
-        </button>
+        <Link to="/fileUpload" href="/fileUpload">
+          <button
+            className={
+              navbar
+                ? "py-2 px-3 text-sm font-medium hover:bg-gray-100 rounded-2xl"
+                : "py-2 px-3 text-sm font-medium hover:bg-gray-50 hover:bg-opacity-20  rounded-2xl"
+            }
+          >
+            Become a host
+          </button>
+        </Link>
         <button>
           <div
             className={
