3c5442a2da395995c16699643d9a2feb6815b678
coscoaj1 <coscoanthony1@gmail.com>
2021-11-25 18:59:33 -0500

    lighthouse

--- components/GuestsDropDown.js
+++ components/GuestsDropDown.js
@@ -8,11 +8,14 @@ import CancelX from "../public/images/x-svgrepo-com (1).svg";
 function GuestsDropDown({ sendTotal }) {
   const [totalGuests, setTotalGuests] = useState(null);
   return (
-    <div className="grid grid-cols-3 h-24 ">
+    <div className="grid h-24 grid-cols-3 ">
       <Popover>
-        <Popover.Button className="grid grid-cols-2 w-24 ">
-          <div className="col-span-1 text-left px-4 bg-opacity-40 ">
-            <div className="py-2 h-7 col-span-1 text-xs font-medium tracking-wider">
+        <Popover.Button
+          aria-label="select guest count"
+          className="grid w-24 grid-cols-2 "
+        >
+          <div className="col-span-1 px-4 text-left bg-opacity-40 ">
+            <div className="col-span-1 py-2 text-xs font-medium tracking-wider h-7">
               Guests
             </div>
             {(() => {
@@ -21,13 +24,13 @@ function GuestsDropDown({ sendTotal }) {
                   return <div className="h-full text-sm font-light">Add</div>;
                 case 1:
                   return (
-                    <div className="text left font-medium text-sm w-24">
+                    <div className="w-24 text-sm font-medium text left">
                       {totalGuests} Guest
                     </div>
                   );
                 default:
                   return (
-                    <div className="text-left font-medium text-sm w-24">
+                    <div className="w-24 text-sm font-medium text-left">
                       {totalGuests} Guests
                     </div>
                   );
@@ -35,12 +38,12 @@ function GuestsDropDown({ sendTotal }) {
             })()}
           </div>
         </Popover.Button>
-        <Popover.Panel className="flex flex-col w-96 bg-white rounded-3xl  text-base transform translate-y-4 -translate-x-52 ">
+        <Popover.Panel className="flex flex-col text-base transform translate-y-4 bg-white w-96 rounded-3xl -translate-x-52 ">
           <div className="flex justify-between px-6 py-4">
             <div>
               <ul>
                 <li>Adults</li>
-                <li className="font-light text-sm text-gray-500">
+                <li className="text-sm font-light text-gray-500">
                   Ages 13 or above
                 </li>
               </ul>
@@ -54,7 +57,7 @@ function GuestsDropDown({ sendTotal }) {
             <div>
               <ul>
                 <li>Children</li>
-                <li className="font-light text-sm text-gray-500">Ages 2-12</li>
+                <li className="text-sm font-light text-gray-500">Ages 2-12</li>
               </ul>
             </div>
             <GuestsCounter
@@ -66,7 +69,7 @@ function GuestsDropDown({ sendTotal }) {
             <div>
               <ul>
                 <li>Infants</li>
-                <li className="font-light text-sm text-gray-500">Under 2</li>
+                <li className="text-sm font-light text-gray-500">Under 2</li>
               </ul>
             </div>
             <GuestsCounter
@@ -79,20 +82,20 @@ function GuestsDropDown({ sendTotal }) {
       {totalGuests ? (
         <div className="ml-5">
           <button
-            className="w-6 h-6 bg-gray-200 hover:bg-gray-300 rounded-full transform translate-y-4"
+            className="w-6 h-6 transform translate-y-4 bg-gray-200 rounded-full hover:bg-gray-300"
             onClick={() => setTotalGuests(null)}
           >
             <CancelX width={18} height={18} className="rounded-full pl-1.5" />
           </button>
         </div>
       ) : null}
-      <div className=" col-start-3  mr-1">
+      <div className="col-start-3 mr-1 ">
         <button
           type="submit"
           onClick={() => {
             sendTotal(totalGuests);
           }}
-          className="relative w-12 h-12 pt-1 active:scale-90 transform duration-200"
+          className="relative w-12 h-12 pt-1 duration-200 transform active:scale-90"
         >
           <Image width={108} height={108} src={searchIcon} alt="search icon" />
         </button>
