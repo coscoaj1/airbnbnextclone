f0962a9572850684ed246cde8560a16d7ffc4b2e
coscoaj1 <coscoanthony1@gmail.com>
2021-11-25 18:31:02 -0500

    meta tags

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,22 @@
 import { SessionProvider } from "next-auth/react";
 import Footer from "../components/Footer";
 import "../styles/globals.css";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps: { session, ...pageProps } }) {
   return (
-    <SessionProvider session={session}>
-      <Component {...pageProps} />
-      <Footer />
-    </SessionProvider>
+    <>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta name="description" content="Airbnb Clone Project." />
+        <title>Airbnb Clone Project</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <SessionProvider session={session}>
+        <Component {...pageProps} />
+        <Footer />
+      </SessionProvider>
+    </>
   );
 }
 
