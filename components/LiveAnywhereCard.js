d4db94c087a7d4480e9658d539067234ff8b5da9
coscoaj1 <coscoanthony1@gmail.com>
2021-11-27 19:49:45 -0500

    mobile

--- components/LiveAnywhereCard.js
+++ components/LiveAnywhereCard.js
@@ -1,25 +1,25 @@
-import React from 'react';
-import Image from 'next/image';
+import React from "react";
+import Image from "next/image";
 
 function LiveAnywhereCard({ card }) {
-	return (
-		<div
-			id="card"
-			className=" row-span-full col-auto font-bold h-auto"
-			key={card.name}
-		>
-			<div>
-				<Image
-					className="rounded md:h-auto md:w-full h-auto w-full sm:h-52"
-					src={card.img}
-					alt=""
-					width={500}
-					height={500}
-				/>
-			</div>
-			<p className="text-lg text-center sm:text-left">{card.title}</p>
-		</div>
-	);
+  return (
+    <div
+      id="card"
+      className="h-auto col-auto font-bold  row-span-full"
+      key={card.name}
+    >
+      <div>
+        <Image
+          className="w-full h-auto rounded md:h-auto md:w-full sm:h-48"
+          src={card.img}
+          alt=""
+          width={500}
+          height={500}
+        />
+      </div>
+      <p className="text-lg text-center sm:text-left">{card.title}</p>
+    </div>
+  );
 }
 
 export default LiveAnywhereCard;
